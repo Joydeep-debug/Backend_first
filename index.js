af84755064614920c6ac70b93c6c767eae042cc9
Joydeep-debug <joydeepchkrbrt99@gmail.com>
2025-06-02 12:52:10 +0530

    FIrst commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const {initializeDatabase} = require("./db/db.connect");
+const Movie = require("./models/movie.models");
+initializeDatabase();
+
+//find a movie with a particular title
+async function readMovieByTitle(movieTitle){
+    try{
+        const movie = await Movie.findOne({title: movieTitle});
+        console.log(movie);
+    } catch(error){
+        throw error
+    }
+}
+
+//readMovieByTitle("PK");
+
+// to get all the movies in the database
+
+async function readAllMovies(){
+    try{
+        const allMovies = await Movie.find();
+        console.log(allMovies);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+//readAllMovies();
+
+// get movie by director name
+async function readMovieByDirector(directorName){
+    try{
+        const movieByDirector = await Movie.find({director: directorName})
+        console.log(movieByDirector);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+readMovieByDirector("Kabir Khan");
